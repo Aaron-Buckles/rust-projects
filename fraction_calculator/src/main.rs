9a86dd016fd08f4a9a36c324432bec1e3740c7e5
Aaron Buckles <abuck351@gmail.com>
2021-09-06 11:01:00 -0700

    Implements subtraction

--- fraction_calculator/src/main.rs
+++ fraction_calculator/src/main.rs
@@ -1,6 +1,6 @@
 use std::fmt::{Display, Debug, Formatter, Result as FmtResult};
 use std::cmp::Ordering;
-use std::ops::Add;
+use std::ops::{Add, Sub};
 
 // Another option would be the Binary GCD algorithm (https://en.wikipedia.org/wiki/Binary_GCD_algorithm)
 fn gcd(mut a: u32, mut b: u32) -> u32 {
@@ -61,6 +61,21 @@ impl Add for Fraction {
     }
 }
 
+impl Sub for Fraction {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self::Output {
+        if self.denominator == other.denominator {
+            Fraction::new(self.numerator - other.numerator, self.denominator)
+        } else {
+            Fraction::new(
+                self.numerator * other.denominator - other.numerator * self.denominator,
+                self.denominator * other.denominator
+            )
+        }
+    }
+}
+
 fn main() {
     let f1 = Fraction::new(1, 4);
     let f2 = Fraction::new(2, 4);
@@ -74,4 +89,8 @@ fn main() {
     let mut f7 = f4 + f5 + f6;
     f7.simplify();
     println!("{} + {} + {} = {}", f4, f5, f6, f7);
+
+    let mut f8 = f2 - f1;
+    f8.simplify();
+    println!("{} - {} = {}", f2, f1, f8);
 }
