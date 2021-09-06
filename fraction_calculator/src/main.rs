a2d72a5cd2576435e36aa7e22523682de0d359af
Aaron Buckles <abuck351@gmail.com>
2021-09-06 11:15:50 -0700

    Allow for negatives
    Also simplifies Sub overload and simplifying fractions with negative denominators

--- fraction_calculator/src/main.rs
+++ fraction_calculator/src/main.rs
@@ -1,9 +1,18 @@
 use std::fmt::{Display, Debug, Formatter, Result as FmtResult};
 use std::cmp::Ordering;
-use std::ops::{Add, Sub};
+use std::ops::{Add, Sub, Neg};
 
 // Another option would be the Binary GCD algorithm (https://en.wikipedia.org/wiki/Binary_GCD_algorithm)
-fn gcd(mut a: u32, mut b: u32) -> u32 {
+fn gcd(mut a: i32, mut b: i32) -> i32 {
+    // Convert a and b to positive integers
+    if a < 0 {
+        a *= -1;
+    }
+    if b < 0 {
+        b *= -1;
+    }
+
+    // Calculate the GCD
     while a != b {
         match a.cmp(&b) {
             Ordering::Less => b -= a,
@@ -16,13 +25,13 @@ fn gcd(mut a: u32, mut b: u32) -> u32 {
 
 #[derive(Debug, Clone, Copy)]
 struct Fraction {
-    numerator: u32,
-    denominator: u32,
+    numerator: i32,
+    denominator: i32,
 }
 
 // TODO: Support negative fractions (need to use i32)
 impl Fraction {
-    pub fn new(numerator: u32, denominator: u32) -> Self {
+    pub fn new(numerator: i32, denominator: i32) -> Self {
         Self {
             numerator,
             denominator,
@@ -33,6 +42,11 @@ impl Fraction {
         let gcd = gcd(self.numerator, self.denominator);
         self.numerator /= gcd;
         self.denominator /= gcd;
+
+        if self.denominator < 0 {
+            self.numerator *= -1;
+            self.denominator *= -1;
+        }
     }
 }
 
@@ -65,14 +79,15 @@ impl Sub for Fraction {
     type Output = Self;
 
     fn sub(self, other: Self) -> Self::Output {
-        if self.denominator == other.denominator {
-            Fraction::new(self.numerator - other.numerator, self.denominator)
-        } else {
-            Fraction::new(
-                self.numerator * other.denominator - other.numerator * self.denominator,
-                self.denominator * other.denominator
-            )
-        }
+        self + -other
+    }
+}
+
+impl Neg for Fraction {
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        Fraction::new(-self.numerator, self.denominator)
     }
 }
 
@@ -90,7 +105,14 @@ fn main() {
     f7.simplify();
     println!("{} + {} + {} = {}", f4, f5, f6, f7);
 
-    let mut f8 = f2 - f1;
-    f8.simplify();
-    println!("{} - {} = {}", f2, f1, f8);
+    let f8 = Fraction::new(1, 4);
+    let f9 = Fraction::new(5, 4);
+    let mut f10 = f8 - f9;
+    f10.simplify();
+    println!("{} - {} = {}", f8, f9, f10);
+
+    let f11 = Fraction::new(-4, -8);
+    let mut f12 = f11;
+    f12.simplify();
+    println!("{} = {}", f11, f12);
 }
