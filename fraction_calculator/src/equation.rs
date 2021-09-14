82623ae0335282a71629b82f561037ea573cd73d
Aaron Buckles <abuck351@gmail.com>
2021-09-13 21:47:23 -0700

    Adds a few RPN unit tests

--- fraction_calculator/src/equation.rs
+++ fraction_calculator/src/equation.rs
@@ -99,23 +99,26 @@ impl Equation {
         for token in tokens {
             match token {
                 Token::Number(_) => output_queue.push(*token),
-                Token::Operator(op1) => {
-                    while let Some(Token::Operator(op2)) = operator_stack.last() {
-                        if op2.precedence() > op1.precedence() {
-                            if let Some(top_token) = operator_stack.pop() {
-                                output_queue.push(top_token);
-                            } else {
-                                return Err(ParseEquationError);
-                            }
-                        } else {
+                Token::Operator(current_operator) => {
+                    while let Some(Token::Operator(top_operator)) = operator_stack.last() {
+                        if current_operator.precedence() > top_operator.precedence() {
+                            // If the current_operator has higher precedence, then just place it
+                            // onto the operator_stack
                             break;
+                        } else if let Some(top_token) = operator_stack.pop() {
+                            // If the current_operator has lower precedence, then push the operator
+                            // on the top of the stack to the output_queue
+                            output_queue.push(top_token);
+                        } else {
+                            return Err(ParseEquationError); // Something went wrong
                         }
                     }
-                    operator_stack.push(*token);
+                    operator_stack.push(*token); // Always push the current_operator onto the stack
                 },
             }
         }
 
+        // At this point the operator_stack should be sorted by highest precedence to lowest
         while operator_stack.len() > 0 {
             if let Some(top_token) = operator_stack.pop() {
                 output_queue.push(top_token);
@@ -123,5 +126,38 @@ impl Equation {
         }
 
         Ok(output_queue)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::equation::{Equation, Token, OperatorType};
+    use super::Fraction;
+    use itertools::Itertools;
+
+    #[test]
+    fn rpn1() {
+        let tokens = vec![
+            Token::Number(Fraction::new(3, 1)),
+            Token::Operator(OperatorType::Add),
+            Token::Number(Fraction::new(4, 1)),
+        ];
+        let result = Equation::shunting_yard_algorithm(&tokens).unwrap().iter().join(" ");
+        let expected = "3 4 +";
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn rpn2() {
+        let tokens = vec![
+            Token::Number(Fraction::new(2, 3)),
+            Token::Operator(OperatorType::Add),
+            Token::Number(Fraction::new(5, 8)),
+            Token::Operator(OperatorType::Mul),
+            Token::Number(Fraction::new(-8, 7)),
+        ];
+        let result = Equation::shunting_yard_algorithm(&tokens).unwrap().iter().join(" ");
+        let expected = "2/3 5/8 -8/7 * +";
+        assert_eq!(result, expected);
     }
 }
